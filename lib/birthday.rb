823dc51aa4fd4483805b4ff604d051eed73bfb7b
Stef Spears <spears.sj87@gmail.com>
2019-09-10 20:17:23 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -12,4 +12,6 @@ end
 
 def age_appropriate_birthday(birthday_kids)
   birthday_kids.each do |kids_name, age|
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age 
+    puts "Happy Birthday #{kids_name}! You are now #{age} years old!" if age <= 12
+  end
+end
